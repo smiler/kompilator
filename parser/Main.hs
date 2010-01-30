4156cc5a69d8a169c9344db6709139087afc79a6
Samuel Strand <samuel.strand@gmail.com>
2010-01-30 20:26:14 +0100

    output the whole AST rather than just a simple test

--- parser/Main.hs
+++ parser/Main.hs
@@ -3,8 +3,5 @@ module Main where
 import UCParser
 
 main = do
-	s <- ucParser
-	putStrLn $ concatMap (\s -> case s of FUNDEC _ id _ _ _ -> show id ++ "{}\n"
-                                              EXTERN _ id _ -> show id ++ ";\n"
-                                              GLOBAL v -> case v of SCALARDEC _ id -> show id ++ "\n"
-                                                                    ARRAYDEC _ id _ -> show id ++ "\n") s
+  s <- ucParser
+  putStrLn $ concatMap (\s -> show s ++ "\n") s
