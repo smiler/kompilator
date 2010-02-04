5d9d01f48406b390ad35ae0a40521d9260be8814
Samuel Strand <samuel.strand@gmail.com>
2010-02-04 03:48:42 +0100

    byggde om lite och bytte alla namn for the lulz

--- semantic/UCSemantic.hs
+++ semantic/UCSemantic.hs
@@ -1,49 +1,97 @@
-module UCSemantic (buildSymTable, SymData(..), ucSemantic) where
+module UCSemantic (Symbol(..), ucSemantic) where
 
 import qualified UCParser as Parser
 import qualified UCParserAST as AST
 
+data Symbol
+  = FUN String AST.Type [Symbol]
+  | SCALAR String AST.Type
+  | ARRAY String AST.Type
+  deriving (Show, Eq)
 
-data SymData
-  = FUNDEC String AST.Type [AST.Vardec] -- Function declaration
-  | EXTERN String AST.Type [AST.Vardec] -- Extern function declaration
-  | GLOBAL String AST.Vardec -- Global variable declaration
-  deriving (Show)
+symName (FUN id _ _) = id
+symName (SCALAR id _) = id
+symName (ARRAY id _) = id
 
-getIdFromSym (FUNDEC id _ _) = id
-getIdFromSym (EXTERN id _ _) = id
-getIdFromSym (GLOBAL id _) = id
+symType (FUN _ t _) = t
+symType (SCALAR _ t) = t
+symType (ARRAY _ t) = t
 
--- Add symbol to symbol table (replace with pushSym below)
-updateSym sym s = (s : sym)
+-- used at top level
+add :: Symbol -> [[Symbol]] -> [[Symbol]]
+add s [] = [[s]]
+add s (ss'@(s':_):ss's) =
+  if (symName s == symName s') then
+      error $ "redeclaration of " ++ symName s
+  else
+    ss' : add s ss's
 
--- Push symbol to symbol table
-pushSym :: [[SymData]] -> SymData -> [[SymData]]
-pushSym [] sym = [[sym]]
-pushSym ((l @ x:xs):syms) sym =
-  if (id(l) == id(sym)) then 
-    [(sym : [x]) ++ xs] ++ syms
-  else 
-    [l] : pushSym syms sym
-      where id = getIdFromSym
+-- used in functions
+push :: Symbol -> [[Symbol]] -> [[Symbol]]
+push s [] = [[s]]
+push s (ss'@(s':_):ss's) =
+  if (symName s == symName s') then
+    (s : ss') : ss's
+  else
+    ss' : push s ss's
 
--- Build symbol table
-buildSymTable [] sym     = sym
-buildSymTable (x:xs) sym =
-  case x of 
-    AST.FUNDEC t id args _ _ 
-      -> buildSymTable xs (updateSym sym (FUNDEC id t args))
-    AST.EXTERN t id args
-      -> buildSymTable xs (updateSym sym (EXTERN id t args))
-    AST.GLOBAL t
-      -> buildSymTable xs (updateSym sym (GLOBAL (id t) t))
-        where
-          id = AST.getIdFromVardec
+lol st = error $ show st
 
--- Validate symbol table
--- Todo: Do something          
-checkSymTable xs = True
+topLevel :: [AST.Topdec] -> [[Symbol]] -> [[Symbol]]
+topLevel [] st = st
+topLevel (d:ds) st
+  = let st' = case d of
+                AST.FUNDEC t id args decs body
+                  -- Hur fan gör man saker sekvensiellt på ett snyggt sätt?
+                  -> let s = add (FUN id t (funArgs args)) st
+                         l = funTimes id body (funDecs decs (funDecs args s))
+                     in if (s == l) then s else s -- bara för att tvinga eval av l
+                AST.EXTERN t id args
+                  -> add (FUN id t (funArgs args)) st
+                AST.GLOBAL (AST.SCALARDEC t id)
+                  -> add (SCALAR id t) st
+                AST.GLOBAL (AST.ARRAYDEC t id _)
+                  -> add (ARRAY id t) st
+    in topLevel ds st'
+
+funArgs :: [AST.Vardec] -> [Symbol]
+funArgs [] = []
+funArgs (d:ds) =
+  case d of -- återanvänder Symbol för enkelhetens skull
+    AST.SCALARDEC t id -> (SCALAR id t) : funArgs ds
+    AST.ARRAYDEC t id _ -> (ARRAY id t) : funArgs ds
+
+funDecs :: [AST.Vardec] -> [[Symbol]] -> [[Symbol]]
+funDecs [] st = st
+funDecs (d:ds) st =
+  case d of
+    AST.SCALARDEC t id -> funDecs ds (push (SCALAR id t) st)
+    AST.ARRAYDEC t id _ -> funDecs ds (push (ARRAY id t) st)
+
+funTimes :: String -> [AST.Stmt] -> [[Symbol]] -> [[Symbol]]
+funTimes id [] st = st
+funTimes id (s:ss) st =
+  case s of
+    AST.EMPTY -> funTimes id ss st
+--    AST.EXPR e -> do{ exprCheck e st
+--               ; funTimes id ss st }
+    AST.EXPR e -> return (exprCheck e st) >> funTimes id ss st
+    AST.IF e s1 Nothing -> return (exprCheck e st) >> funTimes id ss (stmtCheck s1 st)
+    AST.IF e s1 (Just s2) -> return (exprCheck e st) >> funTimes id ss (stmtCheck s2 (stmtCheck s1 st))
+    AST.WHILE e s1 -> return (exprCheck e st) >> funTimes id ss (stmtCheck s1 st)
+    AST.RETURN Nothing -> funTimes id ss (typeCheck id AST.VOID st)
+    -- lolol char = int 
+    AST.RETURN (Just e) -> funTimes id ss (typeCheck id (exprCheck e st) st)
+    AST.BLOCK s1s -> funTimes id ss (funTimes id s1s st)
+---}
+
+exprCheck :: AST.Expr -> [[Symbol]] -> AST.Type
+exprCheck _ _ = AST.INT
+stmtCheck :: AST.Stmt -> [[Symbol]] -> [[Symbol]]
+stmtCheck _ st = st
+typeCheck :: String -> AST.Type -> [[Symbol]] -> [[Symbol]]
+typeCheck _ _ st = st
 
 ucSemantic = do
   s <- Parser.ucParser
-  return (buildSymTable s [])
+  return (topLevel s [])
