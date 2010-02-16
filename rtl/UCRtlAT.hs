21f537e91c6085f7a982fd784a454de053b4aba7
Samuel Strand <samuel.strand@gmail.com>
2010-02-16 15:06:18 +0100

    deth

--- rtl/UCRtlAT.hs
+++ rtl/UCRtlAT.hs
@@ -1,4 +1,7 @@
-module UCRtlAT (Program(..)) where
+module UCRtlAT (Program(..), Dec(..), Insn(..), Expr(..), Binop(..),
+       Unop(..), Relop(..), Temp, Label, Ty(..), rv, fp, sizeof) where
+
+import Prelude hiding (LT, EQ, GT)
 
 data Ty	 -- for memory load/store operations
   = BYTE -- char 
@@ -10,10 +13,10 @@ instance Show Ty where
 type Temp = Int	    -- pseudo register 
 type Label = String -- label
 
-RV :: Temp -- temp for return value 
-RV = 0
-FP :: Temp -- frame pointer temp, for storage of local arrays 
-FP = 1
+rv :: Temp -- temp for return value 
+rv = 0
+fp :: Temp -- frame pointer temp, for storage of local arrays 
+fp = 1
 
 data Relop 
   = LT | LE | EQ | NE | GE | GT
@@ -28,7 +31,7 @@ instance Show Relop where
 data Unop
   = LOAD Ty	-- load value of type 'ty' from address in operand 
 instance Show Unop where
-  show LOAD ty = "load" ++ show ty
+  show (LOAD ty) = "load" ++ show ty
 
 data Binop
   = ADD | SUB | MUL | DIV
@@ -48,11 +51,11 @@ data Expr
     -- binary operators 
   | BINARY Binop Temp Temp
 instance Show Expr where
-  show TEMP t = show t
-  show ICON i = show i
-  show LABREF l = show l
-  show UNARY op src = show op ++ " " ++ show src
-  show BINARY bop src1 src2 = show bop ++ " " ++ show src1 ++ " " ++ show src2
+  show (TEMP t) = show t
+  show (ICON i) = show i
+  show (LABREF l) = show l
+  show (UNARY op src) = show op ++ " " ++ show src
+  show (BINARY bop src1 src2) = show bop ++ " " ++ show src1 ++ " " ++ show src2
 
 type MaybeTemp = Maybe Temp
 
@@ -67,18 +70,18 @@ data Insn
   | EVAL Temp Expr
     -- function calls: could be expr but result temp is optional 
   | CALL MaybeTemp Label [Temp]
-instance Show Expr where
-  show LABDEF l = show l ++ ":\n"
-  show JUMP l = "\t(goto " ++ show l ++ ")\n"
-  show CJUMP op src1 src2 l  = "\t(if (" ++ show op ++ " " ++ 
-                               show src1 ++ " " ++ show src2 ++
-                               ") (goto " ++ show l ++ "))\n"
-  show STORE ty dst src = "\t(store " ++ show ty ++ " " ++ 
+instance Show Insn where
+  show (LABDEF l) = show l ++ ":\n"
+  show (JUMP l) = "\t(goto " ++ show l ++ ")\n"
+  show (CJUMP op src1 src2 l) = "\t(if (" ++ show op ++ " " ++ 
+                              show src1 ++ " " ++ show src2 ++
+                              ") (goto " ++ show l ++ "))\n"
+  show (STORE ty dst src) = "\t(store " ++ show ty ++ " " ++ 
                           show dst ++ " " ++ show src ++ ")\n"
-  show EVAL dst expr = "\t(set " ++ show l ++ " " ++ show expr ++ ")\n"
-  show CALL (Just dst) l args = "\t(set " ++ show dst ++ " (call " ++ show l ++
+  show (EVAL dst expr) = "\t(set " ++ show dst ++ " " ++ show expr ++ ")\n"
+  show (CALL (Just dst) l args) = "\t(set " ++ show dst ++ " (call " ++ show l ++
                                 " " ++ unwords (map show args) ++ ")\n"
-  show CALL Nothing l args = "\t(call " ++ show l ++ 
+  show (CALL Nothing l args) = "\t(call " ++ show l ++ 
                              " " ++ unwords (map show args) ++ ")\n"
     
 data Dec
@@ -90,17 +93,17 @@ data Dec
   | DATA {label :: Label, 
           size :: Int}
 instance Show Dec where
-  show PROC = "\n(procedure " ++ show label ++
-              "\n\t(formals " ++ unwords $ map show formals) ++
-              ")\n\t(locals " ++ unwords $ map show locals ++
-              ")\n\t(frameSize " ++ show frameSize ++ ")\n" ++
-              unwords $ map show insns ++ ")\n"
-  show DATA = "\n(data " ++ show label ++ " " ++ show size ++ "\n"
+  show p@(PROC{}) = "\n(procedure " ++ show (label p) ++
+              "\n\t(formals " ++ (unwords $ map show (formals p)) ++
+              ")\n\t(locals " ++ (unwords $ map show (locals p)) ++
+              ")\n\t(frameSize " ++ show (frameSize p) ++ ")\n" ++
+              (unwords $ map show (insns p)) ++ ")\n"
+  show d@(DATA{}) = "\n(data " ++ show (label d) ++ " " ++ show (size d) ++ "\n"
 
 data Program
   = PROGRAM [Dec]
 instance Show Program where
-  show PROGRAM ds = unwords $ map show ds
+  show (PROGRAM ds) = unwords $ map show ds
 --newLabel :: a -> Label
 --newTemp :: a -> Temp
 
