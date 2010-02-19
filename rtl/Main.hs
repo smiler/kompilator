702328646312a4f77566f772e665ef5c0c8885cf
Samuel Strand <samuel.strand@gmail.com>
2010-02-19 11:31:34 +0100

    olol

--- rtl/Main.hs
+++ rtl/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import UCParser
+import UCRtlAT
 import UCRtl
 
 main = do
+  tree <- ucParser
+  let PROGRAM rtl = parse tree
+  putStrLn $ concatMap show rtl
 --  syms <- ucSemantic
 --  putStrLn $ concatMap (\x -> show x ++ "\n") syms
-  putStrLn "olololol"
-  
+--  putStrLn "olololol"
